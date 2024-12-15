7d532ec24d263764600111810d0818dff8d4e270
Mohakobi79 <mohakobi79@gmail.com>
2024-12-15 20:48:03 +0100

    Update _allfunc.js

--- plugins/_allfunc.js
+++ plugins/_allfunc.js
@@ -2,29 +2,29 @@ import axios from 'axios'
 let handler = m => m
 handler.all = async function (m) {
 
-		global.fetchBuffer = async (url, options) => {
-	try {
-		options ? options : {}
-		const res = await axios({
-			method: "GET",
-			url,
-			headers: {
+                global.fetchBuffer = async (url, options) => {
+        try {
+                options ? options : {}
+                const res = await axios({
+                        method: "GET",
+                        url,
+                        headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
-				'DNT': 1,
-				'Upgrade-Insecure-Request': 1
-			},
-			...options,
-			responseType: 'arraybuffer'
-		})
-		return res.data
-	} catch (err) {
-		return err
-	}
+                                'DNT': 1,
+                                'Upgrade-Insecure-Request': 1
+                        },
+                        ...options,
+                        responseType: 'arraybuffer'
+                })
+                return res.data
+        } catch (err) {
+                return err
+        }
 }
 
 // buat ambil json
 
-		global.fetchJson = async (url, options) => {
+                global.fetchJson = async (url, options) => {
     try {
         options ? options : {}
         const res = await axios({
